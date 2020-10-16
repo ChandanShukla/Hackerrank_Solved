1eaebf2c55cfb842a3c921776d3430ee57301cc3
ChandanShukla <chandanshukla269@gmail.com>
2020-10-16 13:38:31 +0530

    Update String_Formatting.py

--- String_Formatting.py
+++ String_Formatting.py
@@ -3,21 +3,26 @@ def print_formatted(number):
     octal = [0]*50
     hexa = [0]*50
     binary = [0]*100
-    temp=0
+    temp=number
     i=1
+    le=len(bin(number))
+    le=le-1
     while i<=number:
         temp=i
         j=0
-        print(i,end=" ")
+        
+        print(str(i).rjust(le-1),end="")
         while temp!=0:
             octal[j]=temp%8
             temp=int(temp/8)
             j=j+1
+        oc=""
         for k in range(j-1,-1,-1):
-            print(octal[k],end="")
+            #print(octal[k],end="")
+            oc=oc+str(octal[k])
+        print(oc.rjust(le),end="")
         j=0
         temp=i
-        print(" ",end="")
         while temp!=0:
             hexa[j]=temp%16
             
@@ -37,16 +42,29 @@ def print_formatted(number):
             temp=int(temp/16)
             j=j+1
             
-            k=j-1
-            print("k",k,end=" ")
-            while k>-1:
-                if hexa[k]==0 and k==j-1:
-                    print("",end="")
-                else:
-                    print("hex[",k,"]= ",end="")
-                    print(hexa[k],end="")
-                k=k-1
-        print("")
-        i=i+1
+        k=j-1
+        he=""
+        #print("k",k,end=" ")
+        while k>-1:
+            if hexa[k]==0 and k==j-1:
+                print("",end="")
+            else:
+                #print("hex[",k,"]= ",end="")
+                #print(hexa[k],end="")
+                he=he+str(hexa[k])
+            k=k-1
+        print(he.rjust(le),end="")
+        j=0
+        temp=i
+        while(temp!=0):
+            binary[j]=temp%2
+            temp=int(temp/2)
+            j=j+1
+        b=""
+        for k in range(j-1,-1,-1):
+            #print(binary[k],end="")
+            b=b+str(binary[k])
+        print(b.rjust(le))
+        i=i+1 
 #n = int(input())
 print_formatted(50)
