4524b7beed57101cd8660522446a6c8e6e77e5b2
ChandanShukla <chandanshukla269@gmail.com>
2020-10-18 01:05:19 +0530

    Create Iterator_Product.py

---
+++ Iterator_Product.py
@@ -0,0 +1,13 @@
+from itertools import product
+from array import array
+
+arr=[]
+arr=input()
+n=list(map(int,arr.split()))
+arr2=input()
+n1=list(map(int,arr2.split()))
+
+temp=(product(n,n1))
+
+for i in temp:
+    print(i,end=" ")
