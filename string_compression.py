7b55610e38afb4ddeea77bf3cc729caddab2f34d
ChandanShukla <chandanshukla269@gmail.com>
2020-10-20 23:44:06 +0530

    Update string_compression.py

--- string_compression.py
+++ string_compression.py
@@ -1,2 +1,10 @@
-from itertools import groupby
+from itertools import product 
 
+a=input().split()
+n=map(int,a.split())
+b=input().split()
+n1=map(int,b.split())
+
+p=list(product(n,n1))
+
+print(p)
